d99d0926596cac74d2b56ca2914171c8832bef38
M.Aasher <145186889+M-Aasher@users.noreply.github.com>
2025-10-31 20:59:57 +0500

    Add files via upload

---
+++ Electricity Bill Calculator.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+
+int main()
+{
+int units, unit_price, bill;
+char type;
+printf("Enter h for house consumer and c for commercial user:");
+scanf(" %c", &type);
+printf("Enter the amount of units consumed:");
+scanf("%d", &units);
+
+if(units<=200) {
+  if(type=='h')
+      unit_price = 10;
+  else if(type=='c')
+      unit_price = 12;
+}
+else if(units>200 && units<=400) {
+  if(type=='h')
+      unit_price = 12;
+  else if(type=='c')
+      unit_price = 15;
+}
+else {
+   if(type=='h')
+      unit_price = 15;
+   if(type=='c')
+      unit_price = 20;
+}
+bill = units*unit_price;
+printf("Your electricity bill of this month is %d. \n", bill);
+return 0;
+
+
+
+}
